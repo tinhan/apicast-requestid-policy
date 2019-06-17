9c31dcfba8ddf9d443dc8882d1043dc2499439a8
tinhanc <tinhanc@gmail.com>
2019-06-17 16:35:14 +0700

    fixd bug

--- policies/requestid/0.1/rq_script.lua
+++ policies/requestid/0.1/rq_script.lua
@@ -1,35 +1,50 @@
-local setmetatable = setmetatable
 
 local _M = require('apicast.policy').new('Gen UUID', '0.1')
-local mt = { __index = _M }
+local new = _M.new
 
-function _M.new()
-  return setmetatable({}, mt)
+local ngx_var_new_header = ''
+
+function _M.new(config)
+  local self = new(config)
+  local header_setval = config.gen_request_header
+  ngx.log(0, 'get vakue fron header', header_setval)
+
+  if(header_setval == nil){
+    self.ngx_var_new_header = 'breadcrumbId'
+  }else{
+    self.ngx_var_new_header = header_setval
+  }
+  ngx.log(0, 'get vakue fron header', ngx_var_new_header)
+  
+  return self
 end
 
 function _M:rewrite()
   local config = configuration or {}
   local set_header = config.set_header or {}
   local random = math.random
-  local template ='xxxxxxxxxxxxxxxxxxxxxxxxxxxxyy'
+  local template ='xxxxxxxxxxxxyyxxxxxxxxxxxxxxyy'
   local rq_uuid = string.gsub(template, '[xy]', function (c)
         local v = (c == 'x') and random(0, 0xf) or random(8, 0xb)
         return string.format('%x', v) end)
+
+  local header_val = self.ngx_var_new_header
   
-  ngx.req.set_header('x-request-id', rq_uuid)
-  ngx.log(0, 'In coming request { x-request-id : ', rq_uuid, ', { Body : ', ngx.var.request_body , ' } }')
+  ngx.req.set_header(header_val, rq_uuid)
+  ngx.log(0, 'In coming request { ', header_val, ' : ', rq_uuid, ', { Body : ', ngx.var.request_body , ' } }')
   
 end
 
 function _M:body_filter()
     local resp = ""
-    local rq_uid = ngx.req.get_headers()["x-request-id"]
+    local header_val = self.ngx_var_new_header
+    local rq_uid = ngx.req.get_headers()[header_val]
     ngx.ctx.buffered = (ngx.ctx.buffered or "") .. string.sub(ngx.arg[1], 1, 1000)
     if ngx.arg[2] then
       resp = ngx.ctx.buffered
     end
 
-    ngx.log(0, 'Out going response { x-request-id : ', rq_uid, ', { Body : ', resp , ' } }')
+    ngx.log(0, 'Out going response { ',header_val,' : ', rq_uid, ', { Body : ', resp , ' } }')
   
 end
 
